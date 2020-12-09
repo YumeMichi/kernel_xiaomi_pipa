d49e875b47a9827e4dfd33f638883ff8bb9e296c
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-09 00:04:57 -0800

    iommu: msm: Rewrite to improve clarity and performance
    This scope of this driver's lock usage is extremely wide, leading to
    excessively long lock hold times. Additionally, there is lots of
    excessive linked-list traversal and unnecessary dynamic memory
    allocation in a critical path, causing poor performance across the
    board.
    
    Fix all of this by greatly reducing the scope of the locks used and by
    significantly reducing the amount of operations performed when
    msm_dma_map_sg_attrs() is called. The entire driver's code is overhauled
    for better cleanliness and performance.
    
    Note that ION must be modified to pass a known structure via the private
    dma_buf pointer, so that the IOMMU driver can prevent races when
    operating on the same buffer concurrently. This is the only way to
    eliminate said buffer races without hurting the IOMMU driver's
    performance.
    
    Some additional members are added to the device struct as well to make
    these various performance improvements possible.
    
    This also removes the manual cache maintenance since ION already handles
    it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I8c19a1b3d11a25b9c6246a42e41f604e98799558

--- include/linux/device.h
+++ include/linux/device.h
@@ -1126,6 +1126,9 @@ struct device {
 	bool			dma_coherent:1;
 #endif
 
+	struct list_head	iommu_map_list;
+	struct mutex		iommu_map_lock;
+
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
