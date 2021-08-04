0349880e066b403252b6adc9447a9e115ffc11e5
Yu Zhao <yuzhao@google.com>
2021-08-04 01:31:34 -0600

    FROMLIST: mm: x86, arm64: add arch_has_hw_pte_young()
    Some architectures automatically set the accessed bit in PTEs, e.g.,
    x86 and arm64 v8.2. On architectures that do not have this capability,
    clearing the accessed bit in a PTE usually triggers a page fault
    following the TLB miss of this PTE (to emulate the accessed bit).
    
    Being aware of this capability can help make better decisions, e.g.,
    whether to spread the work out over a period of time to reduce bursty
    page faults when trying to clear the accessed bit in many PTEs.
    
    Note that theoretically this capability can be unreliable, e.g.,
    hotplugged CPUs might be different from builtin ones. Therefore it
    should not be used in architecture-independent code that involves
    correctness, e.g., to determine whether TLB flushes are required (in
    combination with the accessed bit).
    Moto-CRs-Fixed:(CR)
    
    Link: https://lore.kernel.org/r/20220309021230.721028-2-yuzhao@google.com/
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Reviewed-by: Barry Song <baohua@kernel.org>
    Acked-by: Brian Geffon <bgeffon@google.com>
    Acked-by: Jan Alexander Steffens (heftig) <heftig@archlinux.org>
    Acked-by: Oleksandr Natalenko <oleksandr@natalenko.name>
    Acked-by: Steven Barrett <steven@liquorix.net>
    Acked-by: Suleiman Souhlal <suleiman@google.com>
    Acked-by: Will Deacon <will@kernel.org>
    Tested-by: Daniel Byrne <djbyrne@mtu.edu>
    Tested-by: Donald Carr <d@chaos-reins.com>
    Tested-by: Holger Hoffstätte <holger@applied-asynchrony.com>
    Tested-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>
    Tested-by: Shuang Zhai <szhai2@cs.rochester.edu>
    Tested-by: Sofia Trinh <sofia.trinh@edi.works>
    Tested-by: Vaibhav Jain <vaibhav@linux.ibm.com>
    Bug: 228114874
    Change-Id: Ie81175d7e0d239f688d31487b298cf9b4fb66707
    Reviewed-on: https://gerrit.mot.com/2268695
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2481002

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -820,6 +820,7 @@ extern pgd_t tramp_pg_dir[PTRS_PER_PGD];
 
 extern int kern_addr_valid(unsigned long addr);
 
+#define arch_has_hw_pte_young	cpu_has_hw_af
 
 void pgd_cache_init(void);
 #define pgtable_cache_init	pgd_cache_init
