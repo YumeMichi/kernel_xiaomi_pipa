1e2c9af87070e54d8e3e597e6c2e96a8d093b6e6
Sultan Alsawaf <sultan@kerneltoast.com>
2021-09-26 00:29:51 -0700

    mm: Move slab shrinkers into a dedicated thread called kshrinkd
    Slab shrinkers are unique in that they have no bearing on direct reclaim
    throttling and reclaim statistics, meaning that they can be completely
    decoupled from the kswapd and direct reclaim paths, making them faster.
    Further justification for this is that shrinkers may be slow and aren't
    guaranteed to free any memory, which means that they can waste kswapd
    and direct reclaim time without producing results. Running the same
    shrinker concurrently can also produce unwanted lock contention inside
    the shrinker itself.
    
    An empirical test of checking how long kswapd and kshrinkd ran on a
    memory constrained system showed that kshrinkd ran for about 10% of the
    duration that kswapd ran, which indicates significant savings.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [ Port to 4.19 ]
    Signed-off-by: Kazuki Hashimoto <kazukih0205@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2819,9 +2819,6 @@ static bool shrink_node(pg_data_t *pgdat, struct scan_control *sc)
 			shrink_node_memcg(pgdat, memcg, sc, &lru_pages);
 			node_lru_pages += lru_pages;
 
-			shrink_slab(sc->gfp_mask, pgdat->node_id, memcg,
-					sc->priority);
-
 			/* Record the group's reclaim efficiency */
 			vmpressure(sc->gfp_mask, memcg, false,
 				   sc->nr_scanned - scanned,
@@ -4016,6 +4013,43 @@ kswapd_try_sleep:
 	return 0;
 }
 
+static int kshrinkd(void *pgdat)
+{
+	pg_data_t *p = pgdat;
+
+	/* This is technically a kswapd thread */
+	current->flags |= PF_KSWAPD;
+	set_freezable();
+	while (1) {
+		unsigned int pri = DEF_PRIORITY;
+		bool stop;
+
+		wait_event_freezable(p->kshrinkd_wait,
+				     (stop = kthread_should_stop()) ||
+				     atomic_long_read(&kshrinkd_waiters));
+		if (unlikely(stop))
+			break;
+
+		/* Shrink slabs on both kswapd and direct reclaimers' behalf */
+		while (1) {
+			struct mem_cgroup *memcg = NULL;
+
+			do {
+				shrink_slab(GFP_KERNEL, p->node_id, memcg, pri);
+			} while ((memcg = mem_cgroup_iter(NULL, memcg, NULL)));
+
+			if (!atomic_long_read(&kshrinkd_waiters))
+				break;
+
+			/* Iterate down each possible priority and then wrap */
+			pri = (pri - 1) % (DEF_PRIORITY + 1);
+		}
+	}
+	current->flags &= ~PF_KSWAPD;
+
+	return 0;
+}
+
 /*
  * A zone is low on free memory or too fragmented for high-order memory.  If
  * kswapd should reclaim (direct reclaim is deferred), wake it up for the zone's
@@ -4066,6 +4100,7 @@ void wakeup_kswapd(struct zone *zone, gfp_t gfp_flags, int order,
 	trace_mm_vmscan_wakeup_kswapd(pgdat->node_id, classzone_idx, order,
 				      gfp_flags);
 	wake_up_interruptible(&pgdat->kswapd_wait);
+	wake_up_interruptible(&pgdat->kshrinkd_wait);
 }
 
 #ifdef CONFIG_HIBERNATION
@@ -4208,6 +4243,16 @@ int kswapd_run(int nid)
 
 	nr_threads = kswapd_threads;
 	for (hid = 0; hid < nr_threads; hid++) {
+		pgdat->kshrinkd[hid] = kthread_run(kshrinkd, pgdat, "kshrinkd%d", nid, hid);
+		if (IS_ERR(pgdat->kshrinkd[hid])) {
+			/* failure at boot is fatal */
+			BUG_ON(system_state < SYSTEM_RUNNING);
+			pr_err("Failed to start kshrinkd on node %d\n", nid);
+			ret = PTR_ERR(pgdat->kshrinkd[hid]);
+			pgdat->kshrinkd[hid] = NULL;
+			return ret;
+		}
+
 		pgdat->kswapd[hid] = kthread_run(kswapd, pgdat, "kswapd%d:%d",
 							nid, hid);
 		if (IS_ERR(pgdat->kswapd[hid])) {
@@ -4217,6 +4262,8 @@ int kswapd_run(int nid)
 				hid, nid);
 			ret = PTR_ERR(pgdat->kswapd[hid]);
 			pgdat->kswapd[hid] = NULL;
+			kthread_stop(pgdat->kshrinkd[hid]);
+			pgdat->kshrinkd[hid] = NULL;
 		}
 	}
 	kswapd_threads_current = nr_threads;
@@ -4230,14 +4277,20 @@ int kswapd_run(int nid)
 void kswapd_stop(int nid)
 {
 	struct task_struct *kswapd;
+	struct task_struct *kshrinkd;
 	int hid;
 	int nr_threads = kswapd_threads_current;
 
 	for (hid = 0; hid < nr_threads; hid++) {
 		kswapd = NODE_DATA(nid)->kswapd[hid];
+		kshrinkd = NODE_DATA(nid)->kshrinkd[hid];
 		if (kswapd) {
 			kthread_stop(kswapd);
 			NODE_DATA(nid)->kswapd[hid] = NULL;
+		}
+		if (kshrinkd) {
+			kthread_stop(kshrinkd);
+			NODE_DATA(nid)->kshrinkd[hid] = NULL;
 		}
 	}
 }
