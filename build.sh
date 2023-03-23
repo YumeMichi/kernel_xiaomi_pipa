7e10dcf8b8f05b7356b4f65c1edc17b7c88e5829
Yuan Si <do4suki@gmail.com>
2023-03-23 10:36:16 +0800

    build: Add a simple build script
    Change-Id: I4d57e79de5df3849d82924fcec58da1251460209
    Signed-off-by: Yuan Si <do4suki@gmail.com>

---
+++ build.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Number of parallel jobs to run
+THREAD="-j$(nproc)"
+
+# LLVM 21.0.0 (https://github.com/Mandi-Sa/clang/releases/tag/amd64-kernel-arm_static-21)
+CLANG_BIN="/mnt/ssd0/aospa/prebuilts/clang-standalone/bin"
+
+# Environment
+export PATH="$CLANG_BIN:$PATH"
+export KBUILD_BUILD_USER=nobody
+export KBUILD_BUILD_HOST=android-build
+
+# Vars
+ARCH="arm64"
+OUT="out"
+
+KMAKE_FLAGS=(
+    -j"$(nproc)"
+    ARCH="$ARCH"
+    O="$OUT"
+
+    LLVM=1
+    LLVM_IAS=1
+
+    CC="clang"
+    CLANG_TRIPLE="aarch64-linux-gnu-"
+)
+
+# Kernel defconfig
+DEFCONFIG="vendor/pipa_user_defconfig"
+
+# Functions
+function clean_all {
+    echo
+    git clean -fdx > /dev/null 2>&1
+}
+
+function make_kernel {
+    clang -v
+    make "${KMAKE_FLAGS[@]}" $DEFCONFIG savedefconfig
+    make "${KMAKE_FLAGS[@]}"
+}
+
+DATE_START=$(date +"%s")
+
+echo -e "${green}"
+echo "-----------------"
+echo "Making Kernel:"
+echo "-----------------"
+echo -e "${restore}"
+
+echo
+
+while read -p "Clean stuffs (y/n)? " cchoice
+do
+case "$cchoice" in
+    y|Y )
+        clean_all
+        echo
+        echo "All Cleaned now."
+        break
+        ;;
+    n|N )
+        break
+        ;;
+    * )
+        echo
+        echo "Invalid try again!"
+        echo
+        ;;
+esac
+done
+
+echo
+
+while read -p "Start building (y/n)? " dchoice
+do
+case "$dchoice" in
+    y|Y )
+        make_kernel
+        break
+        ;;
+    n|N )
+        echo
+        echo "Abort!"
+        echo
+        break
+        ;;
+    * )
+        echo
+        echo "Invalid try again!"
+        echo
+        ;;
+esac
+done
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
