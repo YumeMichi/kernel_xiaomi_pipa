madmax7896/android_kernel_xiaomi_sm8250

drivers: soc: qcom: Import Xiaomi changes
* From dagu-s-oss

Change-Id: Ib1a9f3dbe22a9ba673ed141615256f531cce8af7

drivers/soc/qcom/fsa4480-i2c.c
@@ -356,6 +356,9 @@ static int fsa4480_probe(struct i2c_client *i2c,
 {
 	struct fsa4480_priv *fsa_priv;
 	int rc = 0;
+#ifdef CONFIG_TARGET_PRODUCT_MUNCH
+	union power_supply_propval mode;
+#endif
 
 	fsa_priv = devm_kzalloc(&i2c->dev, sizeof(*fsa_priv),
 				GFP_KERNEL);
@@ -403,6 +406,19 @@ static int fsa4480_probe(struct i2c_client *i2c,
 		  fsa4480_usbc_analog_work_fn);
 
 	BLOCKING_INIT_NOTIFIER_HEAD(&fsa_priv->fsa4480_notifier);
+
+#ifdef CONFIG_TARGET_PRODUCT_MUNCH
+	/* set usbc_mode initial value */
+	rc = power_supply_get_property(fsa_priv->usb_psy,
+			POWER_SUPPLY_PROP_TYPEC_MODE, &mode);
+	if (rc) {
+		dev_err(fsa_priv->dev, "%s: Uable to read USB TYPEC_MODE during probe: %d\n",
+				__func__, rc);
+	} else {
+		atomic_set(&(fsa_priv->usbc_mode), mode.intval);
+		dev_info(fsa_priv->dev, "%s: set usbc_mode to %d\n", __func__, fsa_priv->usbc_mode.counter);
+	}
+#endif
 
 	return 0;
 

YumeMichi/kernel_xiaomi_pipa

drivers/soc/qcom/fsa4480-i2c.c
@@ -356,6 +356,9 @@ static int fsa4480_probe(struct i2c_client *i2c,
 {
 	struct fsa4480_priv *fsa_priv;
 	int rc = 0;
+#ifdef CONFIG_MACH_XIAOMI_MUNCH
+	union power_supply_propval mode;
+#endif
 
 	fsa_priv = devm_kzalloc(&i2c->dev, sizeof(*fsa_priv),
 				GFP_KERNEL);
@@ -403,6 +406,19 @@ static int fsa4480_probe(struct i2c_client *i2c,
 		  fsa4480_usbc_analog_work_fn);
 
 	BLOCKING_INIT_NOTIFIER_HEAD(&fsa_priv->fsa4480_notifier);
+
+#ifdef CONFIG_MACH_XIAOMI_MUNCH
+	/* set usbc_mode initial value */
+	rc = power_supply_get_property(fsa_priv->usb_psy,
+			POWER_SUPPLY_PROP_TYPEC_MODE, &mode);
+	if (rc) {
+		dev_err(fsa_priv->dev, "%s: Uable to read USB TYPEC_MODE during probe: %d\n",
+				__func__, rc);
+	} else {
+		atomic_set(&(fsa_priv->usbc_mode), mode.intval);
+		dev_info(fsa_priv->dev, "%s: set usbc_mode to %d\n", __func__, fsa_priv->usbc_mode.counter);
+	}
+#endif
 
 	return 0;
 