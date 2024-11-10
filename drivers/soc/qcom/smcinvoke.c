37251400bdd5a22bfff8bc6b4a2d08ed8d80f835
Juhyung Park <qkrwngud825@gmail.com>
2024-11-11 01:10:28 +0900

    smcinvoke: Silence meaningless warning
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/soc/qcom/smcinvoke.c
+++ drivers/soc/qcom/smcinvoke.c
@@ -1598,7 +1598,7 @@ static long process_accept_req(struct file *filp, unsigned int cmd,
 		 * waiting for new cb requests, hence return EAGAIN here
 		 */
 		if (!cb_txn) {
-			pr_err("%s txn %d either invalid or removed from Q\n",
+			pr_debug("%s txn %d either invalid or removed from Q\n",
 					__func__, user_args.txn_id);
 			ret = -EAGAIN;
 			goto out;
