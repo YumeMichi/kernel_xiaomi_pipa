e4fe5847e5617f32b911e171aa5fa8b060dbeaac
Sean Du <do4suki@gmail.com>
2026-08-04 02:43:17 +0800

    mm: mglru: use mmap lock helpers
    Adapt the MGLRU page table walk to the mmap_lock API.
    
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3619,13 +3619,13 @@ static void walk_mm(struct lruvec *lruvec, struct mm_struct *mm, struct lru_gen_
 			break;
 
 		/* the caller might be holding the lock for write */
-		if (down_read_trylock(&mm->mmap_sem)) {
+		if (mmap_read_trylock(mm)) {
 			unsigned long start = walk->next_addr;
 			unsigned long end = mm->highest_vm_end;
 
 			err = walk_page_range(start, end, &args);
 
-			up_read(&mm->mmap_sem);
+			mmap_read_unlock(mm);
 
 			if (walk->batched) {
 				spin_lock_irq(&pgdat->lru_lock);
